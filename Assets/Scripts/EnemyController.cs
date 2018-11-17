8952569228ea722dfd0dac92c019417a3f6dd464
Beniox <beniox01@gmail.com>
2018-11-17 22:13:50 +0000

    Advanced Movement of the Enemy

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,18 +5,65 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour {
 
     private float distance;
+    private Vector3 direction = new Vector3(1,0,0);
+    private System.Random random = new System.Random();
+    private System.DateTime directionChange;
+    public Rect MovementRadius;
 	// Use this for initialization
 	void Start () {
-		
+        Debug.Log("start");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(1 * Time.deltaTime, 0, 0);
+        Vector3 movement = new Vector3(0,0,0);
+        if (!IsInArea()) ChangeDirection();
+        movement = direction.Scale(1 * Time.deltaTime);
+
+
+
+            transform.Translate(movement);
     }
 
-    void OnCollisionEnter(Collision col)
+    void ChangeDirection()
     {
-        
+        direction = GetRandomDirection();
+        directionChange = System.DateTime.Now;
+    }
+
+     bool IsInArea()
+    {
+        float x = transform.position.x;
+        float y = transform.position.y;
+        if (x < MovementRadius.xMax && x > MovementRadius.xMin && y < MovementRadius.yMax && y > MovementRadius.yMin) return true;
+        else return false;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        ChangeDirection();
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if((System.DateTime.Now -directionChange).TotalMilliseconds > 1000)
+        {
+            ChangeDirection();
+        }
+    }
+
+    Vector3 GetRandomDirection ()
+    {
+        int ran = random.Next(4);
+        Debug.Log(ran);
+        switch (ran)
+        {
+            case 0: return new Vector3(1, 0);
+            case 1: return new Vector3(-1, 0);
+            case 2: return new Vector3(0, 1);
+            case 3: return new Vector3(0, -1);
+            default: return new Vector3(0, 0);
+        }
+
     }
 }
---
+++ Assets/Scripts/VectorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public static class VectorExtensions
+{
+    public static Vector3 Scale(this Vector3 vector, float factor)
+    {
+        Vector3 result = new Vector3(vector.x * factor, vector.y *factor, vector.z * factor);
+        return result;
+    }
+}
+
